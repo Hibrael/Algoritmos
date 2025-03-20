c3b1cc0de03f2834285318e68402247543b49c49
Hibrael <hibraelxavier@gmail.com>
2025-03-20 19:48:55 -0300

    Fim do desenvolviemnto da App HotelSonoBom.cpp

--- ListaOficial02/HotelSonoBom.cpp
+++ ListaOficial02/HotelSonoBom.cpp
@@ -15,10 +15,9 @@ main(){
     // f) O valor consumido no frigobar.
     // g) Número de serviços de massagens utilizadas pelo cliente.
     // h) O valor do consumo de bar.
-    int numeroreserva, hospedes, massagens, tipoquarto, qtddiarias;
-    string nome;
-    float valorquarto, valorfamilia, valorfrigobar, valormassagens, valorbar, valormassagens, adicionalhospedes, valorfinal, valortotaldiarias, valortotalservicos, valortotal;
-
+    int numeroreserva, hospedes, massagens, tipoquarto, qtddiarias, manutencaofrigobar, adicionalhospedes, valormassagens;
+    string nome, quartostring;
+    float valorquarto, valorfrigobar, valorbar, valorgarçom, valortotaldiarias, totalfrigobar, valortotalservicos, taxaISS, valorISS, valortotal, totalbar;
 
     /*=======================ENTRADA=======================*/
 
@@ -47,7 +46,7 @@ main(){
     cout<<"Número de massagens utilizadas: ";
     cin>>massagens;
 
-    cout<<"Valor de consumido no bar: ";
+    cout<<"Valor consumido no bar: ";
     cin>>valorbar;
 
     /*=======================PROCESSO=======================*/
@@ -56,23 +55,22 @@ main(){
     if (tipoquarto == 1)
     {
         valorquarto = 50.00;
-
+        quartostring = "Standard";
     }else if (tipoquarto == 2)
     {
         valorquarto = 80.00;
-
-    }else if (tipoquarto == 3)
+        quartostring = "Luxo";
+    }else
     {
         valorquarto = 150.00;
+        quartostring = "VIP";
     }
-    
-    
 
     //IF QUANTIDADE HOSPEDES
-    if (hospedes >2 || hospedes<5)
+    if (hospedes >2 && hospedes<5) //familia pequena
     {
         adicionalhospedes = qtddiarias * 20.00;
-    }else if (hospedes >=5)
+    }else if (hospedes >=5) //familia grande
     {
         adicionalhospedes = qtddiarias * 40.00;
     }
@@ -88,11 +86,13 @@ main(){
     //ADIÇÃO DE TAXA AOS VALORES CONSUMIDOS NO BAR E FRIGOBAR.
     if (valorfrigobar>0)
     {
-        valorfrigobar += 12.00;
+        manutencaofrigobar = 12;
+        totalfrigobar = valorfrigobar + manutencaofrigobar;
     }
     if (valorbar>0)
     {
-        valorbar += (valorbar*0.1);
+        valorgarçom = valorbar*0.1;
+        totalbar = valorbar + valorgarçom;
     }
 
     /*==============SOMA FINAL DOS VALORES==============*/
@@ -104,16 +104,37 @@ main(){
                                                                         //adicional em função do número de hóspedes 
     
     //SOMA SERVIÇOS
-    valortotalservicos = valormassagens + valorfrigobar + valorbar;
-
-    //SOMA FINAL
-    valortotal=valortotaldiarias + valortotalservicos;
+    valortotalservicos = valormassagens + totalfrigobar + totalbar;
 
     //CALCULO DA TAXA ISS
 
-    if (qtddiarias>5)
+    if (qtddiarias>=5 && qtddiarias<=10)
     {
-        /* code */
+        taxaISS = 0.03;
+    }else if (qtddiarias>10){
+        taxaISS = 0.01;
+    }else{
+        taxaISS = 0.05;
     }
+     //SOMA FINAL
+     valortotal = valortotaldiarias + valortotalservicos;
+     valorISS = valortotal * taxaISS;
+     valortotal += valorISS;
+     /*=======================SAÍDA=======================*/
     
+     cout << "\n==========Detalhes da Reserva==========\n";
+    cout << "Número da reserva: " << numeroreserva << endl;
+    cout << "Responsável pela reserva: " << nome << endl;
+    cout << "Tipo de quarto: " << quartostring << endl;
+    cout << "Número de diárias: " << qtddiarias << endl;
+    cout << "Valor das diárias: R$" << valortotaldiarias << endl;
+    cout << "Valor total do serviço de massagem: R$" << valormassagens << endl;
+    cout << "Valor consumido do frigobar: R$" << valorfrigobar << endl;
+    cout << "Valor de manutenção do frigobar: R$" << manutencaofrigobar << endl;
+    cout << "Valor total do serviço de bar: R$" << valorbar << endl;
+    cout << "Taxa de comissão ao garçom (10%): R$" << valorgarçom << endl;
+    cout << "ISS: R$" << valorISS << endl;
+    cout << "Conta final: R$" << valortotal << endl;
+
+    return 0;
 }
